416b5f7e928017899a2c5089ab778092c2619ebc
Maged Maher <dev.magedmaher@gmail.com>
2021-04-01 19:12:51 +0200

    implement react-navigation with home and qa screens

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from './screens/home';
+import QAScreen from './screens/qa';
 
+
+const Stack = createStackNavigator();
 
 const App = () => {
-  return null;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="home" headerMode='none'>
+        <Stack.Screen name="home" component={HomeScreen} />
+        <Stack.Screen name="qa" component={QAScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
 
 
---
+++ src/screens/qa.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+
+const QAScreen = () => {
+  return (
+    <Text>QA Screen</Text>
+  )
+}
+
+
+export default QAScreen;
