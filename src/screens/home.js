2bdc3a5d0e35f10fe1ad69002c47d9a09f6d9110
Maged Maher <dev.magedmaher@gmail.com>
2021-04-01 19:15:34 +0200

    update home screen with stylish welcome text

--- src/screens/home.js
+++ src/screens/home.js
@@ -1,12 +1,27 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
 
 
 const HomeScreen = () => {
   return (
-    <Text>Home Screen</Text>
-  )
+    <View style={styles.container}>
+      <Text style={styles.text}>تحدث للبدأ..</Text>
+    </View>
+  )  
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 36,
+    fontWeight: 'bold',
+    color: '#555'
+  },
+});
 
 
 export default HomeScreen;
