c0b6f7605ca83c8909e8edb8e2df81d4cf725736
Maged Maher <dev.magedmaher@gmail.com>
2021-04-01 19:44:53 +0200

    add ibm-watson service to connect to the api

---
+++ src/services/ibm-watson.js
@@ -0,0 +1,12 @@
+import axios from 'axios'
+
+const host = __DEV__ ? 'http://192.168.1.97:3000/api' : 'https://nfla1-rest-api.herokuapp.com/api';
+
+
+const assistant = inputText => {
+  console.log({ inputText });
+  return axios.post(host + '/ibm-watson/assistant', { inputText });
+}
+
+
+export { assistant };
