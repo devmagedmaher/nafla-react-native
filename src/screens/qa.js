5c56e78b8356d80f7f55d11a8ee42852dc9472d4
Maged Maher <dev.magedmaher@gmail.com>
2021-04-01 19:43:17 +0200

    list Q&A in qa screen

--- src/screens/qa.js
+++ src/screens/qa.js
@@ -1,12 +1,49 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { View, FlatList, StyleSheet, Text } from 'react-native';
+import QAList from '../constants/qa-list';
 
 
 const QAScreen = () => {
   return (
-    <Text>QA Screen</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>يمكنك سؤالي أحد الأسئلة التالية:-</Text>
+      <FlatList
+        data={QAList}
+        renderItem={({ item }) => (
+          <Text style={styles.listItem}>{item.text}</Text>
+        )}
+        keyExtractor={item => item.id.toString()}
+      />
+    </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'stretch',
+    justifyContent: 'center',
+    paddingTop: 50,
+  },
+  listItem: {
+    textAlign: 'center',
+    fontSize: 18,
+    color: '#666',
+    borderBottomColor: '#ddd',
+    borderBottomWidth: 2,
+    marginVertical: 10,
+    paddingBottom: 7,
+  },
+  title: {
+    alignSelf: 'center',
+    fontSize: 22,
+    color: '#333',
+    marginBottom: 50,
+    padding: 5,
+    borderBottomWidth: 0.5,
+    borderBottomColor: '#999',
+  },
+});
 
 
 export default QAScreen;
